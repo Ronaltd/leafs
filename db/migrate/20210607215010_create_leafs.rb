d697897c94341e6a9bbb249271df7a4bafeafa5c
Ronalt Batista <ronaltdbatista@gmail.com>
2021-06-07 18:51:23 -0300

    create model leaf

---
+++ db/migrate/20210607215010_create_leafs.rb
@@ -0,0 +1,12 @@
+class CreateLeafs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :leafs do |t|
+      t.integer :credit
+      t.string :items
+      t.references :user, null: false, foreign_key: true
+      t.references :dropoff, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_07_214136) do
+ActiveRecord::Schema.define(version: 2021_06_07_215010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,17 @@ ActiveRecord::Schema.define(version: 2021_06_07_214136) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "leafs", force: :cascade do |t|
+    t.integer "credit"
+    t.string "items"
+    t.bigint "user_id", null: false
+    t.bigint "dropoff_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["dropoff_id"], name: "index_leafs_on_dropoff_id"
+    t.index ["user_id"], name: "index_leafs_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -47,4 +58,6 @@ ActiveRecord::Schema.define(version: 2021_06_07_214136) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "leafs", "dropoffs"
+  add_foreign_key "leafs", "users"
 end
