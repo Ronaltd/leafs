cd0c00f8e70ccfae6e4bacb2018d19514cc7ade5
Luciana Mascarenhas <lucianamascarenhas011@hotmail.com>
2021-06-12 00:02:29 -0300

    form front new and leafs count navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  helper_method :leafs
+
+  def leafs
+    @leafs = Leaf.all
+  end
 
   protected
 
