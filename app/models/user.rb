93035f536885cf0faffe942af43ae9673be59016
Wanda Piccin <piccin.wanda@gmail.com>
2021-06-13 10:11:35 -0300

    validação e ajustes

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,9 @@ class User < ApplicationRecord
   has_many :leafs
   has_many :baskets      
   has_one_attached :photo
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true
+  validates :leafs_balance, numericality: { only_integer: true }
 end
 
